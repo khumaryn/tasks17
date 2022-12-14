0d47da6ed76d21ebf50058956a11c1fe2c2fcac4
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-15 02:32:12 +0400

    Add files via upload

---
+++ ikiededitoplamaq/classhome/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace classhome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Verilmiş iki ədədi toplayıb nəticəsini qaytaran metod 
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            var sumcemleri = numberssum(num1, num2);
+            Console.WriteLine(sumcemleri);
+
+        }
+        static int numberssum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
