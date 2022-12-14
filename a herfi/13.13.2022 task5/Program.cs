d65db661270bf6ac06e6eb79fba68a7a84745b02
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-15 02:34:55 +0400

    Add files via upload

---
+++ a herfi/13.13.2022 task5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _13._13._2022_task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Verilmiş string dəyərində a hərfinin olub olmadığını qaytaran metod
+            Console.WriteLine("sozu daxil edin");
+            var word = Console.ReadLine();
+            var resultword = Aletter(word);
+            Console.WriteLine(resultword);
+                
+        }
+        static string Aletter(string word)
+        {
+            bool hasA = false;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a' || word[i] == 'A') 
+                {
+                    hasA = true;
+                    break;
+                }
+
+
+            }
+            if (hasA == true)
+            {
+                Console.WriteLine("A herfi var");
+            }
+            else
+            {
+                Console.WriteLine("A herfi yoxdur");
+            }
+            return word;
+        }
+    }
+}
