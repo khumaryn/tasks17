50ba800dd5cff6f709cdaeb93983fd2e7f60cd12
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-15 02:33:21 +0400

    Add files via upload

---
+++ kvadratiqaytaran/12.13.2022 task 2/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _12._13._2022_task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilmiş ədədin kvadratını qaytaran metod
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            var multiplyResult = multiply(num1);
+
+            Console.WriteLine(multiplyResult);
+
+
+
+        }
+        static int multiply(int num1)
+        {
+            return num1 *num1;
+        }
+        
+    }
+    }
+            
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+
