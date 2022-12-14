b4de59ee636aebfc6ffda341246792db0bd17b0f
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-15 02:38:17 +0400

    Add files via upload

---
+++ ededin_reqemlerinin_cemini_qaytran_metod/13.12.22 task 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _13._12._22_task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        // - Verilmiş ədədin rəqəmləri cəmini qaytaran metod
+            Console.WriteLine("ededi daxil edin");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            var result = Reqemcem(num);
+            Console.WriteLine(result);
+
+        }
+
+
+        static int Reqemcem ( int num )
+        {
+            var sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num = (num - num % 10) / 10;
+
+            }
+            return sum;
+            
+            
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+}    
+
