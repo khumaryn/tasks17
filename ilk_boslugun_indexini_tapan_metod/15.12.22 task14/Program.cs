fbb7fb410d22e28dc8ae4d2fde78ffd778d0e743
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-15 02:36:40 +0400

    Add files via upload

---
+++ ilk_boslugun_indexini_tapan_metod/15.12.22 task14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _15._12._22_task14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(bosluqolmayan(Console.ReadLine()));
+        }
+        ////Verilmiş yazıdaki ilk boşluq olmayan index-i qaytaran metod
+        static int bosluqolmayan(string index)
+        {
+            for (int i = 0; i < index.Length; i++)
+            {
+                if (index[i] != ' ')
+                    return i;
+            }
+            return -1;
+        } 
+}
+}
