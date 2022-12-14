49c81ab28b0b98861d572eff94509cdfe1bc5597
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-15 02:30:28 +0400

    Add files via upload

---
+++ quvvete yuksletme/task 14.12.22/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task_14._12._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Verilmiş ədədin verilmiş qüvvətini tapan metod
+
+            Console.WriteLine("ededi daxil edin");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2= Convert.ToInt32(Console.ReadLine());
+            var resultquvvet = quvvet(num1, num2);
+            Console.WriteLine(resultquvvet);
+
+            
+
+        }
+        static int quvvet( int num1,int num2 )
+        {
+            var quvvet = 1;
+            for(int i=0; i<num2; i++)
+            {
+                quvvet *= num1;
+            }
+            return quvvet;
+           
+        }
+    }
+}
