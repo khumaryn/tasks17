42769fedd0c13b8ede084e3a694180e7bb011e51
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-15 11:09:04 +0400

    Add files via upload

---
+++ 15.12.22 task11/15.12.22 task11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _15._12._22_task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 45, -2, -4 };
+            var newnums = musbetler(numbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(newnums);
+            }
+        }
+        //Verilmiş ədədlər siyahısındaki ədədlərin yalnız müsbət olanlarından ibarət yeni bir array qaytaran metod
+        static int[] musbetler (int[] numbers)
+        {
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+         
+                if (numbers[i]>0)
+                    count++;    
+            }
+            int j = 0;
+            int[] positivenumbers = new int[count];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > 0)
+                {
+                    positivenumbers[j] = numbers[i];
+                    j++;
+                }  
+               
+
+            }
+                return positivenumbers;
+
+        }
+    }
+}
---
+++ 15.15.2022 task15/15.15.2022 task15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _15._15._2022_task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Bosluq(Console.ReadLine()));
+        }
+        //Verilmiş yazıdaki son boşluq olmayan index-i qaytaran metod
+
+        static int Bosluq (string word)
+        {
+            
+            for (int i = 0; i < word.Length; i++)
+            {
+                int last = word.Length-1;
+                if (word[i] != ' ')
+                {
+                    
+                    return last;
+                }
+                return -1;
+
+            }
+        }
+
+    }
+}
